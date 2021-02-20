d3750d1ed272e68e2f8e2ce88bb0e42bb940d934
Duy Truong <41127922+duyhtruong@users.noreply.github.com>
2021-02-20 10:42:42 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+//body-parser simplifies request by parsing the raw request body and giving you req.body
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
-    extended: true,
+    extended:true,
   })
 )
+
+//route looks for a GET request on root /, then returns some JSON
+app.get('/', (request,response)=>{
+  response.json({info:'Node.js, Express, and Postgres API'})
+})
+
+app.listen(port,()=>{
+  console.log(`App running on port ${port}.`)
+})
+
+
+
