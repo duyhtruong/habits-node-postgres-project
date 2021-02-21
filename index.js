3df7bd99780ac8d97326345792413eb58f18b905
Duy Truong <41127922+duyhtruong@users.noreply.github.com>
2021-02-20 20:25:37 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 const db = require('./queries')
+const cors = require("cors")
 
+app.use(cors())
 //body-parser simplifies request by parsing the raw request body and giving you req.body
 app.use(bodyParser.json())
 app.use(
@@ -23,7 +25,7 @@ app.listen(port,()=>{
 
 app.get('/users', db.getUsers)
 app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUsers)
+app.post('/users', db.createUser)
 app.put('/users/:id', db.updateUser)
 app.delete('/users/:id', db.deleteUser)
 
