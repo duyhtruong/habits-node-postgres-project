44ea9dd5a4f1db46cca57f4a864d4f0cb1427c75
Duy Truong <41127922+duyhtruong@users.noreply.github.com>
2021-02-20 20:36:14 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+let main_body = document.getElementById("main-body");
+
 async function displayAllUsers(){
     let response = await fetch('http://localhost:3000/users/');
     let data = await response.json();
-    console.log(data);
+    data.map(function(row){
+        let firstName = document.createElement('p');
+        let email = document.createElement('p');
+
+        firstName.innerHTML = row.name;
+        email.innerHTML = row.email;
+
+        main_body.appendChild(firstName);
+        main_body.appendChild(email);
+    })
+
 }
 
 displayAllUsers();
