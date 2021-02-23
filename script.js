d020bba729cfb6c61adc87740bf9af90d5dc1d2b
Duy Truong <41127922+duyhtruong@users.noreply.github.com>
2021-02-22 20:20:30 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let main_body = document.getElementById("main-body");
 
+
+//Display All Users
 async function displayAllUsers(){
     let response = await fetch('http://localhost:3000/users/');
     let data = await response.json();
@@ -15,5 +17,37 @@ async function displayAllUsers(){
     })
 
 }
-
 displayAllUsers();
+
+function addNewUser(){
+    
+}
+
+
+// ADD NEW USER MODAL
+
+// Get the modal
+var modal = document.getElementById("addModal");
+
+// Get the button that opens the modal
+var addBtn = document.getElementById("addBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+addBtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
