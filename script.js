b4d7d7b9a1eed57384160452d3f928668d1b0470
Duy Truong <41127922+duyhtruong@users.noreply.github.com>
2021-03-10 22:00:59 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ function addNewHabit(userid){
         method: "POST",
         headers: {
             "Accept": "application/json",
-            "Content-Type": 'application/json'
+            "Content-Type": "application/json"
         },
         body: JSON.stringify({
             habitName: userInputHabit,
